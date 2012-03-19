dc621123e81ec8428ae68c3e8c6cf3d738ef8ab0
Martin Marochnic <matoweb@gmail.com>
2012-03-19 22:32:14 +0100

    Fix: Oprava pridavania zadaní.

--- models.rb
+++ models.rb
@@ -34,7 +34,7 @@ class Branch < ActiveRecord::Base
   has_many :users
   has_many :contacts
 
-  scope :with_users, joins('LEFT JOIN users ON users.branch_id = branches.id').where('users.branch_id = branches.id').uniq
+  scope :with_active_users, joins('LEFT JOIN users ON users.branch_id = branches.id').where('users.branch_id = branches.id AND users.is_active = true').uniq
 
   def actual_controlor
     users.where(:is_active => true).last
